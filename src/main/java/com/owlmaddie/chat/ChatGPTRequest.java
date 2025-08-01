93538f4216b12f340567a7202e7b910fbcd71e30
lorisj-elefant <loris@elefant.gg>
2025-08-01 12:31:55 -0700

    removed max tokens so llm works

--- src/main/java/com/owlmaddie/chat/ChatGPTRequest.java
+++ src/main/java/com/owlmaddie/chat/ChatGPTRequest.java
@@ -41,14 +41,14 @@ public class ChatGPTRequest {
         List<ChatGPTRequestMessage> messages;
         ResponseFormat response_format;
         float temperature;
-        int max_tokens;
+        // int max_tokens;
         boolean stream;
 
         public ChatGPTRequestPayload(String model, List<ChatGPTRequestMessage> messages, Boolean jsonMode, float temperature, int maxTokens) {
             this.model = model;
             this.messages = messages;
             this.temperature = temperature;
-            this.max_tokens = maxTokens;
+            // this.max_tokens = maxTokens;
             this.stream = false;
             if (jsonMode) {
                 this.response_format = new ResponseFormat("json_object");
