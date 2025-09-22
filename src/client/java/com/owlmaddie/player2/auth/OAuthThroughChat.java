5ce66238bc619deaec60008e0cf8363f982de20d
lorisj-elefant <loris@elefant.gg>
2025-09-22 12:57:18 -0700

    oauth through chat seems to work

--- src/client/java/com/owlmaddie/player2/auth/OAuthThroughChat.java
+++ src/client/java/com/owlmaddie/player2/auth/OAuthThroughChat.java
@@ -1,5 +1,28 @@
 package com.owlmaddie.player2.auth;
 
-public class OAuthThroughChat {
+import com.owlmaddie.network.ClickEventHelper;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+
+public class OAuthThroughChat {
+        public static void handleAuthError() {
+                Player2OAuthHandler.startOAuthFlow(data -> {
+                        String chatMsg = String.format("To use AI features, please authorize here: %s",
+                                        data.verificationUriComplete);
+                        Minecraft.getInstance().player.displayClientMessage(
+                                        Component.literal(chatMsg)
+                                                        .withStyle(Style.EMPTY
+                                                                        .withClickEvent(ClickEventHelper.openUrl(
+                                                                                        data.verificationUriComplete))),
+                                        false);
+                }, () -> {
+                        Minecraft.getInstance().player.displayClientMessage(
+                                        Component.literal(
+                                                        "Your Player2 API key has been automatically retrieved and saved."
+                                                                        + "You can now use all the mod features!"),
+                                        false);
+                });
+        }
 }
